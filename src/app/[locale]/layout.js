cee823a201eb071fcff1ddf74f77008653db84f8
Hajavololona Armellin <armelgeek5@gmail.com>
2024-05-18 12:07:49 +0300

    internationalisation

---
+++ src/app/[locale]/layout.js
@@ -0,0 +1,21 @@
+import { NextAuthProvider } from "@/components/core/provider/NextAuthProvider";
+import { Toaster } from "@/components/ui/sonner";
+import { poppins } from "@/lib/utils/fonts";
+
+import "@/styles/globals.css";
+
+export default function LocaleLayout({
+  children,
+  params: { locale },
+}) {
+  return (
+    <html lang={locale} className={`${poppins.variable} dark`}>
+      <body className="flex flex-col min-h-screen font-sans antialiased bg-background dark">
+        <NextAuthProvider>
+          {children}
+          <Toaster position="top-center" richColors />
+        </NextAuthProvider>
+      </body>
+    </html>
+  );
+}
